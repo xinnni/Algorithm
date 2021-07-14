55cbfe2f3c171c947037a1164536ef3055547c68
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-14 23:40:50 +0900

    삼각형 판별하기 풀이

---
+++ 2. 삼각형 판별하기/test.js
@@ -0,0 +1,30 @@
+/* solution */
+
+const solution = (a, b, c) => {
+	     let answer = 'YES'
+	     let arr = [a, b, c]
+       let sortArr = arr.sort((a, b) => a - b)
+
+			   if(sortArr[0] + sortArr[1] <= sortArr[2]) {
+             answer = 'NO'
+          } 
+					else {
+           answer
+			  }
+      return answer
+}
+  
+/* develop */
+
+const solution = (a, b, c) => {
+       let answer = 'YES', max
+			 let sum = a + b + c
+
+				if(a > b ) max = a
+				else max = b
+				if(c > max) max = c
+
+				if((sum - max) <= max) answer = 'NO'
+
+				return answer
+     }
