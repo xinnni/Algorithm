cca7da74e71afaa394cebbd22b0b3ec3b2d5b707
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-22 19:59:13 +0900

    대소문자로 변환

---
+++ 13. 대소문자로 변환/readme.md
@@ -0,0 +1,11 @@
+// Getting Started //
+
+대문자와 소문자가 같이 존재하는 문자열을 입력 받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 로직을 구현하라.
+
+// INPUT //
+
+let str = `StuDY`
+
+// OUTPUT //
+
+console.log(solution(str)) -> `sTUdy`
---
+++ 13. 대소문자로 변환/test.js
@@ -0,0 +1,10 @@
+/* solution */
+
+const solution = (str) => {
+	    let anwser = ''
+        for (let el of str) {
+         if(el === el.toUpperCase()) anwser += el.toLowerCase()
+        else anwser += el.toUpperCase()
+     }
+   return anwser
+  }
