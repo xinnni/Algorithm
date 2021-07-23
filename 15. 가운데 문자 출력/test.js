6fc8c2991bd935d0e0fdad30faf778795c6470f3
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-23 13:26:00 +0900

    가운데 문자 출력

---
+++ 15. 가운데 문자 출력/test.js
@@ -0,0 +1,21 @@
+/* solution */
+
+const solution = (str) => {
+	     let answer
+        let mid = parseInt(str.length / 2)
+         if(str.length % 2 !== 0) answer += str.substring(mid, mid + 1)
+         else answer += str.substring(mid - 1, mid + 1)
+
+      return answer
+  }
+  
+/* develop */
+
+const solution = (str) => {
+      let answer
+       let mid = parseInt(str.length / 2)
+        if(str.length % 2 !== 0) answer += str.substr(mid, 1)
+         else answer += str.substr(mid - 1, 2)
+     
+      return answer
+  }
