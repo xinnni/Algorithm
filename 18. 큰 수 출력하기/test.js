dcd61e8d9ca1aeed90c0297f9446b6aebde032a2
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-24 09:28:14 +0900

    큰 수 출력하기

---
+++ 18. 큰 수 출력하기/test.js
@@ -0,0 +1,11 @@
+/* solution */
+
+const solution = (arr) => {
+      let answer = [arr[0]]
+       for(let i = 1; i < arr.length; i++) {
+         if(arr[i] > arr[i - 1]) {
+               answer.push(arr[i])
+         }
+      }
+     return answer
+  }
