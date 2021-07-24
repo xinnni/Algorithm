e689211222ee2bd9c0b02b9a342b13e315260639
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-25 00:10:52 +0900

    점수 계산

---
+++ 21. 점수 계산/test.js
@@ -0,0 +1,31 @@
+/* solution */
+
+const solution = (arr) => {
+      let answer = [], cnt = 0
+        for (let el of arr) {
+         if(el === 1) {
+           cnt++
+           answer.push(cnt)
+	       }
+        else {
+          cnt = 0
+      }
+   }
+	 return answer.reduce((acc, curr) => {
+	   return acc + curr
+	 }, 0)
+}
+  
+/* develop */
+
+const solution = (arr) => {
+      let answer = 0, cnt = 0
+        for (let el of arr) {
+          if(el === 1){
+                cnt++
+                answer += cnt
+         }
+          else cnt = 0
+       }
+    return answer
+  }
