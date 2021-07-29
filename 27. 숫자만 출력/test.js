1238a8ac86725b987f463535f4ca6b2d35fc0176
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-29 11:48:56 +0900

    숫자만 출력

---
+++ 27. 숫자만 출력/test.js
@@ -0,0 +1,27 @@
+/* solution */
+
+const solution = (str) => {
+  const regex = /[^0-9]/g;
+  let answer = parseInt(str.replace(regex, ""));
+  return answer;
+};
+
+/* develop(1) */
+
+const solution = (str) => {
+  let answer = "";
+  for (let el of str) {
+    if (!isNaN(el)) answer += el;
+  }
+  return parseInt(answer);
+};
+
+/* develop(2) */
+
+const solution = (str) => {
+  let answer = 0;
+  for (let el of str) {
+    if (!isNaN(el)) answer *= 10 + Number(el);
+  }
+  return answer;
+};
