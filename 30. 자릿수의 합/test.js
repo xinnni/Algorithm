447524d21f6f28c5a01a1806c096869c1ba6e471
xinnni <79839230+xinnni@users.noreply.github.com>
2021-07-30 10:29:54 +0900

    자릿수의 합

---
+++ 30. 자릿수의 합/test.js
@@ -0,0 +1,41 @@
+/* solution */
+
+const solution = (arr) => {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let el of arr) {
+    let sum = 0,
+      tmp = el;
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = parseInt(tmp / 10);
+    }
+    if (sum > max) {
+      max = sum;
+      answer = el;
+    } else if (sum === max) {
+      if (el > answer) answer = el;
+    }
+  }
+  return answer;
+};
+
+/* develop */
+
+const solution = (arr) => {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let el of arr) {
+    let sum = el
+      .toString()
+      .split("")
+      .reduce((acc, curr) => acc + Number(curr), 0);
+    if (sum > max) {
+      max = sum;
+      answer = el;
+    } else if (sum === max) {
+      if (el > answer) answer = el;
+    }
+  }
+  return answer;
+};
