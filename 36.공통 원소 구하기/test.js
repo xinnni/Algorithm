f4a038029ff86f9d3764fd897d1a4d0d3b1e7100
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-03 09:44:13 +0900

    공통 원소 구하기

---
+++ 36.공통 원소 구하기/test.js
@@ -0,0 +1,18 @@
+/* solution */
+
+const solution = (arr1, arr2) => {
+  let answer = [];
+  arr1.sort();
+  arr2.sort();
+  let p1 = (p2 = 0);
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1]);
+      p1++;
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+  return answer;
+};
