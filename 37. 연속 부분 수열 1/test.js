e9826836a3641749ef2bfda501e04f99b6077320
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-04 09:46:28 +0900

    연속 부분 수열 1

---
+++ 37. 연속 부분 수열 1/test.js
@@ -0,0 +1,16 @@
+/* solution */
+
+function solution(m, arr) {
+  let answer = 0,
+    lt = 0,
+    sum = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === m) answer++;
+    while (sum >= m) {
+      sum -= arr[lt++];
+      if (sum === m) answer++;
+    }
+  }
+  return answer;
+}
