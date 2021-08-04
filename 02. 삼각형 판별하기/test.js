cd9f02274e9daabeecd4cca3494f3d121f845d77
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-04 09:46:53 +0900

    삼각형 판별하기

--- 02. 삼각형 판별하기/test.js
+++ 02. 삼각형 판별하기/test.js
@@ -1,30 +1,30 @@
 /* solution */
 
-const solution = (a, b, c) => {
-	     let answer = 'YES'
-	     let arr = [a, b, c]
-       let sortArr = arr.sort((a, b) => a - b)
+function solution(a, b, c) {
+  let answer = "YES";
+  let arr = [a, b, c];
+  let sortArr = arr.sort((a, b) => a - b);
 
-			   if(sortArr[0] + sortArr[1] <= sortArr[2]) {
-             answer = 'NO'
-          } 
-					else {
-           answer
-			  }
-      return answer
+  if (sortArr[0] + sortArr[1] <= sortArr[2]) {
+    answer = "NO";
+  } else {
+    answer;
+  }
+  return answer;
 }
-  
+
 /* develop */
 
-const solution = (a, b, c) => {
-       let answer = 'YES', max
-			 let sum = a + b + c
+function solution(a, b, c) {
+  let answer = "YES",
+    max;
+  let sum = a + b + c;
 
-				if(a > b ) max = a
-				else max = b
-				if(c > max) max = c
+  if (a > b) max = a;
+  else max = b;
+  if (c > max) max = c;
 
-				if((sum - max) <= max) answer = 'NO'
+  if (sum - max <= max) answer = "NO";
 
-				return answer
-     }
+  return answer;
+}
