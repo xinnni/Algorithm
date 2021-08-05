5cb8a5d04ed69b93f9907da3ccb133e1e082e102
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-05 09:32:17 +0900

    문자 찾기

--- 10. 문자 찾기/test.js
+++ 10. 문자 찾기/test.js
@@ -1,16 +1,16 @@
 /* solution */
 
-const solution = (str, t) => {
-	     let answer = 0;
-          for(let el of str) {
-            if(el === t) answer++
-         }
-       return answer;
+function solution(str, t) {
+  let answer = 0;
+  for (let el of str) {
+    if (el === t) answer++;
+  }
+  return answer;
 }
-  
+
 /* develop */
 
-const solution = (str, t) => {
-      let answer = str.split(t).length
-	   return answer - 1
-  }
+function solution(str, t) {
+  let answer = str.split(t).length;
+  return answer - 1;
+}
