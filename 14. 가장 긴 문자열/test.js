c517dfb138d985589a9346a0d7fc5292ac886b78
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-06 20:07:28 +0900

    가장 긴 문자열

--- 14. 가장 긴 문자열/test.js
+++ 14. 가장 긴 문자열/test.js
@@ -1,22 +1,23 @@
 /* solution */
 
-const solution = (arr) => {
-         let max = arr[0].length;
-           for (let el of arr) {
-             if(max < el.length) max = el
-     }
-		return max
+function solution(arr) {
+  let max = arr[0].length;
+  for (let el of arr) {
+    if (max < el.length) max = el;
   }
-  
+  return max;
+}
+
 /* develop */
 
-const solution = (str) => {
-      let answer, max = Number.MIN_SAFE_INTEGER
-        for (let el of arr) {
-          if(el.length > max) {
-             max = el.length
-             answer = el
-        }
-      }
-    return answer
+function solution(arr) {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let el of arr) {
+    if (el.length > max) {
+      max = el.length;
+      answer = el;
+    }
   }
+  return answer;
+}
