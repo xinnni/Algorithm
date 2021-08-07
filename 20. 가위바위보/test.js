942fa7e0a2231c47881f2d6f78ce760ed5822ab6
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-08 01:01:10 +0900

    가위바위보

--- 20. 가위바위보/test.js
+++ 20. 가위바위보/test.js
@@ -1,30 +1,36 @@
 /* solution */
 
-const solution = (a, b) => {
-      // 1: 가위 a // b: 3
-      // 2: 바위 a // b: 1
-      // 3: 보  a // b: 2      
-       let answer = []
-         for(let i = 0; i < a.length; i++) {
-          if(a[i] === b[i]) answer.push('D')
-           else if(a[i] === 1 && b[i] === 3 ||
-                   a[i] === 2 && b[i] === 1 ||
-                   a[i] === 3 && b[i] === 2) answer.push('A')
-              else answer.push('B')
-       }
-     return answer
+function solution(a, b) {
+  // 1: 가위 a // b: 3
+  // 2: 바위 a // b: 1
+  // 3: 보  a // b: 2
+  let answer = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) answer.push("D");
+    else if (
+      (a[i] === 1 && b[i] === 3) ||
+      (a[i] === 2 && b[i] === 1) ||
+      (a[i] === 3 && b[i] === 2)
+    )
+      answer.push("A");
+    else answer.push("B");
   }
+  return answer;
+}
 
 /* develop */
 
-const solution = (a, b) => {
-      let answer = ''
-         for(let i = 0; i < a.length; i++) {
-          if(a[i] === b[i]) answer += 'D' + ' '
-           else if(a[i] === 1 && b[i] === 3 ||
-                   a[i] === 2 && b[i] === 1 ||
-                   a[i] === 3 && b[i] === 2) answer += 'A' + ' '
-              else answer += 'B' + ' '
-       }
-     return answer.length - 1
+function solution(a, b) {
+  let answer = "";
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) answer += "D" + " ";
+    else if (
+      (a[i] === 1 && b[i] === 3) ||
+      (a[i] === 2 && b[i] === 1) ||
+      (a[i] === 3 && b[i] === 2)
+    )
+      answer += "A" + " ";
+    else answer += "B" + " ";
   }
+  return answer.length - 1;
+}
