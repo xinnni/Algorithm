5d81df21eb8d8092502af2f6159ef5bbef280e7b
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-08 01:01:03 +0900

    보이는 학생

--- 19. 보이는 학생/test.js
+++ 19. 보이는 학생/test.js
@@ -1,12 +1,13 @@
 /* solution */
 
-const solution = (arr) => {
-      let answer = 1, max = arr[0]
-        for (let i = 1; i < arr.length; i++) {
-          if(max < arr[i]) {
-              max = arr[i]
-              answer++
-       }
-     }
-   return answer;
+function solution(arr) {
+  let answer = 1,
+    max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+      answer++;
+    }
   }
+  return answer;
+}
