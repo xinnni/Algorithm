0d89acb49b272d1f7c9038274366ea04bfc2a2f0
xinnni <79839230+xinnni@users.noreply.github.com>
2021-11-10 21:50:45 +0900

    괄호 문자 제거

---
+++ 44. 괄호 문자 제거/readme.md
@@ -0,0 +1,13 @@
+# Getting Started
+
+입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
+
+# INPUT
+
+let str= "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+
+# OUTPUT
+
+```js
+console.log(solution(str)); -> EFLM
+```
---
+++ 44. 괄호 문자 제거/test.js
@@ -0,0 +1,12 @@
+/* solution */
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
+  return answer;
+}
